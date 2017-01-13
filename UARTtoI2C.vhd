b9df5f4b86a67dede1eeb5f1a6dbbf765dfeca35
JasonMur <cjasmurph@gmail.com>
2017-01-12 22:03:47 -0500

    Update UARTtoI2C.vhd

--- UARTtoI2C.vhd
+++ UARTtoI2C.vhd
@@ -1,25 +1,25 @@
 ----------------------------------------------------------------------------------
--- Engineer: 				Jason Murphy
--- Create Date:   		09:00 01/27/2016 
--- Design Name: 			UARTtoI2C
+-- Engineer: 			Jason Murphy
+-- Create Date:   		09:00 01/12/2017 
+-- Design Name: 		UARTtoI2C
 -- Module Name:   		UARTtoI2C - Behavioral 
--- Project Name: 			GPSInterface
+-- Project Name: 		GPSInterface
 -- Target Devices: 		Spartan 6 xc6slx9-3tgg144
 -- Tool versions: 		ISE 14.7
--- Description: 			Reads GPS data from UART via I2C on
---								Raspberry Pi
+-- Description: 		Reads GPS data from UART via I2C on
+--				Raspberry Pi
 -- Dependencies: 			
--- Revision 				0.01 - File Created
+-- Revision 			0.01 - File Created
 -- Additional Comments: 
 ----------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
 entity UARTtoI2C is Port ( 
-	sck : inout  STD_LOGIC;
-   sda : inout  STD_LOGIC;
-   RxD : in  STD_LOGIC;
-   clk50 : in  STD_LOGIC);
+   	sck : inout  STD_LOGIC;
+   	sda : inout  STD_LOGIC;
+   	RxD : in  STD_LOGIC;
+   	clk50 : in  STD_LOGIC);
 end UARTtoI2C;
 
 architecture Behavioral of UARTtoI2C is
@@ -38,17 +38,17 @@ end component;
 component UART Port ( 
 	clk50 : in STD_LOGIC;
 	RxD : in  STD_LOGIC;
-   Dout : out  STD_LOGIC_VECTOR (7 downto 0);
-   Drdy : out  STD_LOGIC);
+   	Dout : out  STD_LOGIC_VECTOR (7 downto 0);
+   	Drdy : out  STD_LOGIC);
 end component;
 
 component fifo port (  
 	clk50 : in std_logic;
-   readData : in std_logic;   --Read Data from FIFO.  Active low data read on falling edge
-   writeData : in std_logic;  --Write Data to FIFO.  Active low data written on falling edge
-   dataOut : out std_logic_vector(7 downto 0);    --output data from FIFO
-   dataIn : in std_logic_vector (7 downto 0);     --input data to FIFO
-   empty : out std_logic);     --set as '1' when FIFO overrun occurs
+   	readData : in std_logic;   --Read Data from FIFO.  Active low data read on falling edge
+   	writeData : in std_logic;  --Write Data to FIFO.  Active low data written on falling edge
+   	dataOut : out std_logic_vector(7 downto 0);    --output data from FIFO
+   	dataIn : in std_logic_vector (7 downto 0);     --input data to FIFO
+   	empty : out std_logic);     --set as '1' when FIFO overrun occurs
 end component;
 
 signal dataFromFIFOSig, dataToFIFOSig : std_logic_vector(7 downto 0);
@@ -68,18 +68,18 @@ I2C1 : I2CInt port map (
 
 fifo1 : fifo port map (
 	clk50 => clk50,
-   readData => readDataSig,
-   writeData => writeDataSig,
-   dataOut => dataFromFIFOSig,
-   dataIn => dataToFIFOSig,
+   	readData => readDataSig,
+   	writeData => writeDataSig,
+   	dataOut => dataFromFIFOSig,
+   	dataIn => dataToFIFOSig,
 	empty => bsySig
 );
 
 UART1 : UART Port map (
 	clk50 => clk50,
 	RxD => RxD,
-   Dout => dataToFIFOSig,
-   Drdy => writeDataSig
+   	Dout => dataToFIFOSig,
+   	Drdy => writeDataSig
 );
 
 end Behavioral;
