8a1660b58210d6eaf76a80f1d9b03197fbbb56d4
JasonMur <cjasmurph@gmail.com>
2017-01-12 22:22:25 -0500

    Update GPSInterface.py

--- GPSInterface.py
+++ GPSInterface.py
@@ -8,10 +8,16 @@ def i2cWrite(busNr, devAddr, regAddr, values):
 def i2cRead(busNr, devAddr, regAddr, numBytes):
     bus = smbus.SMBus(busNr)
     values = bus.read_i2c_block_data(devAddr, regAddr, numBytes)
-    print("Read ", format(int(values[0]), '#04X'), format(int(values[1]), '#04X'))
+    #for i in range(0,16):
+    	#print("Read ", format(int(values[i]), '#04X'))
+	#print(str(unichr(values[i])))
     return values
 
-values = i2cRead(1, 0x62, 0, 4)
+for o in range(0,100):
+    values = i2cRead(1, 0x62, 0, 16)
+    a = ''.join(chr(i) for i in values)
+    print a
+
 
 
 
