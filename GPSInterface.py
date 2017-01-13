ee0c80669e581573037803796aed56c7b642d21e
JasonMur <cjasmurph@gmail.com>
2017-01-13 11:01:26 -0500

    Update GPSInterface.py

--- GPSInterface.py
+++ GPSInterface.py
@@ -10,10 +10,17 @@ def i2cRead(busNr, devAddr, regAddr, numBytes):
     values = bus.read_i2c_block_data(devAddr, regAddr, numBytes)
     return values
 
-for o in range(0,100):
-    values = i2cRead(1, 0x62, 0, 16)
-    a = ''.join(chr(i) for i in values)
-    print a
+value = 0
+values = [0]
+while True:
+    value = i2cRead(1,0x62,0,1)
+    if value == [36]:
+	a = ''.join(chr(i) for i in values)
+	print a
+	values = []
+    else:
+	values.append(value[0])
+   
 
 
 
